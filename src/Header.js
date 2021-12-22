fc25eec788848e49de54138c416a25a66ad2a080
tubi55 <tubi55@nate.com>
2021-12-22 17:22:17 +0900

    기본 헤더 모양

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+function Header(){
+  return (
+    <header>
+      <h1>
+        <a href="#">DCODELAB</a>
+      </h1>
+      
+      <ul id="gnb">
+        <li><a href="#">Department</a></li>
+        <li><a href="#">Flickr</a></li>
+        <li><a href="#">Youtube</a></li>
+        <li><a href="#">Gallery</a></li>
+      </ul>       
+    </header>
+  )
+}
+export default Header;
