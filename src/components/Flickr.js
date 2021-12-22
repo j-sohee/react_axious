c051fe04f8b693df3c0a91f1b9433e53a9f4198c
tubi55 <tubi55@nate.com>
2021-12-22 20:39:10 +0900

    DOM생성전 로딩 처리

--- src/components/Flickr.js
+++ src/components/Flickr.js
@@ -1,6 +1,6 @@
 //npm install axios
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const baseURL = "https://www.flickr.com/services/rest/?";
 const method = "flickr.interestingness.getList";
@@ -9,42 +9,57 @@ const count = 5;
 const url = `${baseURL}method=${method}&api_key=${key}&per_page=${count}&format=json&nojsoncallback=1&privacy_filter=1`;  
 
 
-function Flickr(){  
-
+function Flickr(){   
   let [items, setItems] = useState([]);
+  let [loading, setLoading]  = useState(false);
 
-  axios.get(url)
-  .then(data=>{    
-    const photos = data.data.photos.photo; 
-    setItems(photos)
-  })
-  .catch(err=>{
-    console.error(err);
-  })
+  const getFlickr = async ()=>{
+    try {
+      const data = await axios.get(url)
+        .then(item=>{
+          const photos = item.data.photos.photo;
+          setItems(photos);
+        });
+
+        setLoading(true);
+    }
+    catch (err){
+      console.error(err);
+    }
+  }
+
+  useEffect(()=>{
+    getFlickr();
+  },[]);  
+
+  const Lists = ()=>{
+    return(
+      items.map((data,index)=>{
+        let imgSrc =`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`;
+        let imgSrcBig = `https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`;
+     
+        return (  
+          <article key={index}>
+            <div className="inner">
+              <div className="pic">               
+                <img src={imgSrc} />
+              </div>
+              <p>{data.title}</p>
+            </div>
+          </article>          
+        )
+      })
+    )
+    
+  }
   
   return (
     <main>
       <h1>Flickr Gallery</h1>
 
       <section>
-      {
-        items.map((data,index)=>{
-          let imgSrc =`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`;
-			    let imgSrcBig = `https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`
-          return(            
-            <article key={index}>
-              <div className="inner">
-                <div className="pic">               
-                  <img src={imgSrc} />
-                </div>
-                <p>{data.title}</p>
-              </div>
-            </article>
-          )
-        })
-      } 
-      </section>   
-     
+        {loading ? <Lists /> : <p>로딩중</p>}
+      </section>        
     </main>
   )
 }
